5c9665f70efd416c490185131775465efb9ede02
d3m3sh <jmoucadel@outlook.com>
2025-03-03 15:23:27 +0100

    manage multidomains certbot

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,16 +6,18 @@ else
     staging_arg=""
 fi
 
-domain=$(echo "$DOMAINS" | cut -d',' -f1)
-if [ -d "/etc/letsencrypt/live/$domain" ]; then
-    echo "Certificates already exists, renew checking..."
-    certbot renew
-else
-    echo "First install of certificates..."
-    certbot certonly --webroot --webroot-path=/var/www/certbot \
-        --email "$EMAIL" \
-        --agree-tos \
-        --no-eff-email \
-        $staging_arg \
-        -d $domain
-fi 
+export IFS=","
+for domain in $DOMAINS; do
+    if [ -d "/etc/letsencrypt/live/$domain" ]; then
+        echo "Certificates already exists for $domain, renew checking..."
+        certbot renew
+    else
+        echo "First install of certificates for $domain..."
+        certbot certonly --webroot --webroot-path=/var/www/certbot \
+            --email "$EMAIL" \
+            --agree-tos \
+            --no-eff-email \
+            $staging_arg \
+            -d $domain
+    fi
+done
